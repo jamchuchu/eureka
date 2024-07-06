d12b208194c26ffb82731fa234213f798da9e4b8
장혜민 <gpals5987@gmail.com>
2024-07-06 11:04:05 +0900

    first commit

---
+++ src/main/java/com/sparta/eureka2/Eureka2Application.java
@@ -0,0 +1,13 @@
+package com.sparta.eureka2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eureka2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Eureka2Application.class, args);
+    }
+
+}
